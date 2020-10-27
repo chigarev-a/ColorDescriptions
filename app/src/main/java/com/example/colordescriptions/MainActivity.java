5bf68f5534443194f87db50024af2e1769cedf91
chigarev-a <a.chigarev96@gmail.com>
2020-10-27 22:00:34 +0500

    Создание макета активности

--- app/src/main/java/com/example/colordescriptions/MainActivity.java
+++ app/src/main/java/com/example/colordescriptions/MainActivity.java
@@ -6,13 +6,12 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
 
-    private void test(){
-        int a = 1;
-    }
 }
